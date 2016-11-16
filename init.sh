b4666624f50810ea8b91b72894a0c875db2c7cd8
andre <tanandre93@gmail.com>
2016-11-16 05:29:15 +0000

    edit start script for ubuntu

--- init.sh
+++ init.sh
@@ -8,21 +8,16 @@ git config --global user.email "tanandre93@gmail.com"
 
 # check vim version, and update vimrc, then run :PluginInstall
 sudo apt-get install vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
-source ~/.vimrc
 vim +PluginInstall +qall
 
 # download tmux and update conf file, then run prefix + I
-git clone https://github.com/tmux/tmux.git ~/tmux
-cd tmux
-sh autogen.sh
-./configure && make
-cd ..
-sudo rm -rf tmux
+sudo apt-get install tmux
 cp .tmux.conf ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
-~/.tmux/plugins/tpm/bin/update_plugins all
+~/.tmux/plugins/tpm/bin/install_plugins all
 
 # download zsh, and oh my zsh. then, update zshrc.
 sudo apt-get install zsh
