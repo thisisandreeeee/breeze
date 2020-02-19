06e2e59470b3d84ccd98162b1af4b109ccc64a30
Andre Tan <tanandre93@gmail.com>
2020-02-19 23:45:25 +0800

    [andre] uncomment pyenv installation

--- scripts/setup_pyenv.sh
+++ scripts/setup_pyenv.sh
@@ -2,11 +2,11 @@
 
 ROOT_DIR="$(realpath $(dirname pwd))"
 
-# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-# echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-# echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 
 source ~/.zshrc
 
