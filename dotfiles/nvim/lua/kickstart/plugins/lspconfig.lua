4e432afc62245a441ead77a2468ffc2fe2f660a3
andre <tanandre93@gmail.com>
2024-10-27 16:30:39 +0800

    point pyright to pyenv shim

--- dotfiles/nvim/lua/kickstart/plugins/lspconfig.lua
+++ dotfiles/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -169,7 +169,13 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
+        pyright = {
+          settings = {
+            python = {
+              pythonPath = '~/.pyenv/shims/python',
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
