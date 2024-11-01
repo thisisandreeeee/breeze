9157917cc0264a609d1232658188104a058eb30e
Andre Tan <andre.tan@go-jek.com>
2024-11-01 15:19:19 +0800

    check if node is installed

--- scripts/setup_neovim.sh
+++ scripts/setup_neovim.sh
@@ -2,5 +2,14 @@
 
 ROOT_DIR="$(realpath $(dirname pwd))"
 
-cp -r ${ROOT_DIR}/dotfiles/nvim ~/.config/nvim
+#!/bin/bash
+if type node > /dev/null 2>&1 && which node > /dev/null 2>&1 ;then
+    node -v
+    echo "node is installed, moving nvim configurations"
+    cp -r ${ROOT_DIR}/dotfiles/nvim ~/.config/nvim
+else
+    echo "need to install node first"
+    exit 1
+fi
+
 
